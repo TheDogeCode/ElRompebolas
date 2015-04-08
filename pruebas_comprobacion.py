38877af4b19c0900a53c359d2d204e0d99fa1a04
villoldin <villoldoserranod@hotmail.com>
2015-04-08 12:36:41 +0200

    Update pruebas_comprobacion.py

--- pruebas_comprobacion.py
+++ pruebas_comprobacion.py
@@ -1,39 +1,45 @@
 import random
 from random import randint
 matriz=[]
-matriz=[[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,3,3,3,3,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,1,2,1,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,3,3,3,3,2,1],[1,2,2,2,2,2,2,2,1],[1,1,1,1,1,1,1,1,1]]
+for i in range(9):
+        matriz.append([randint(1,3)for i in range(9)])
 for i in matriz:
     print i
+
 def posicion():
     print""
     print"Introduzca las coordenadas de la posicion:"
-    posicionx=int(raw_input())
     posiciony=int(raw_input())
+    posicionx=int(raw_input())
     print"La posicion escogida es:"
     print matriz[posicionx-1][posiciony-1]
     def compro_abajo():
         a=0
         while matriz[posicionx-1][posiciony-1]==matriz[posicionx][posiciony-1]:
-            print "correcto"
             a+=1
+            print"correcto"
+            print a
             matriz[posicionx+a][posiciony-1]=0
             for i in matriz:
-                print i
-            if a==7:
-                break  
-            ## esto es un comentario
+                print i                     
         posicion()
-    def compro_arriba():
-        a=0
-        while matriz[posicionx-1][posiciony-1]==matriz[posicionx-a][posiciony-1]:
-            a=a+1
-            print"correcto"
-            if a==7:
-                a=0
-                matriz[poscicion-a][posiciony-1]=0
-                for i in matriz:
-                    print i
-            else:
-                posicion()
-    compro_abajo()
+    def compro_arriba():    
+        a=1
+        while matriz[posicionx-1][posiciony-1]==matriz[posicionx-1][posiciony-a]:
+            a+=1
+            print "correcto"
+            matriz[posicionx-1][posiciony-1]=0
+            for i in matriz:
+                print i
+    compro_abajo()       
+    
+    try:
+        compro_abajo()
+    except IndexError():
+        posicion()
+    try:
+        compro_arriba()
+    except IndexError:
+        posicion()
 posicion()
+
