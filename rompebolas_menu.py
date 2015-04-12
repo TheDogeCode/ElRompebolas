b12b967bcc260374fbdae13fda27fe0ab020eef2
villoldin <villoldoserranod@hotmail.com>
2015-04-12 17:29:29 +0200

    Update rompebolas_menu.py

--- rompebolas_menu.py
+++ rompebolas_menu.py
@@ -12,6 +12,91 @@ def menu():
         a=raw_input()
         eleccion=int(a)
         matriz=[]
+        def posicion():
+            print""
+            print"Introduzca las coordenadas de la posicion."
+            print"Fila:"
+            y=int(raw_input())
+            print"Columna:"
+            x=int(raw_input())
+            if x>9:
+                print"Esa posicion no existe"
+                posicion()
+            if y>9:
+                    print"Esa posicion no existe"
+                    posicion()
+            if x<1:
+                    print"Esa posicion no existe"
+                    posicion()
+            if y<1:
+                    print"Esa posicion no existe"
+                    posicion()
+            print"La posicion escogida es:"    
+            print matriz[y-1][x-1]
+            def compro_abajo():
+                a=-1
+                while matriz[y-1][x-1]==matriz[y+a][x-1]:
+                    matriz[y-1][x-1]=0           
+                    matriz[y+a][x-1]=0
+                    print"correcto"
+                    a+=1
+                    if matriz[y+a][x-1]==matriz[y+a+1][x-1]:
+                        matriz[y-1][x-1]=0
+                        matriz[y+a][x-1]=0
+                        matriz[y+a+1][x-1]=0
+                        a+=1                       
+                        for i in matriz:
+                            print i   
+                        posicion()
+            def compro_arriba():
+                while matriz[y-1][x-1]==matriz[y-2][x-1]:
+                    print "correcto"
+                    a=0 #Variable para que se avance la posicion
+                    matriz[y-1][x-1]=0            
+                    matriz[y-a][x-1]=0
+                    a+=1
+                    if matriz[y-a][x-1]==matriz[y-(a+1)][x-1]:
+                        matriz[y-a][x-1]=0
+                        matriz[y-(a+1)][x-1]=0
+                        a+=1
+                    else:            
+                        for i in matriz:
+                            print i
+            def compro_derecha():
+                a=-1
+                while matriz[y-1][x-1]==matriz[y-1][x+a]:
+                    matriz[y-1][x-1]=0           
+                    matriz[y-1][x+a]=0
+                    print"correcto"
+                    a+=1
+                    if matriz[y-1][x+a]==matriz[y-1][x+a+1]:
+                        matriz[y-1][x-1]=0
+                        matriz[y-1][x+a]=0
+                        matriz[y-1][x+a+1]=0
+                        a+=1                       
+                        for i in matriz:
+                            print i 
+                        posicion()
+            def compro_izquierda():
+                a=-1
+                while matriz[y-1][x-1]==matriz[y-1][x-2]:
+                    matriz[y-1][x-1]=0           
+                    matriz[y-1][x-2]=0
+                    print""
+                    a+=1
+                    if matriz[y-1][x-2]==matriz[y-1][x-a-1]:
+                        matriz[y-1][x-1]=0
+                        matriz[y-1][x-a]=0
+                        matriz[y-1][x-a-1]=0
+                        a+=1                       
+                        for i in matriz:
+                            print i 
+                        posicion()
+            try:
+                compro_abajo()
+            except IndexError:
+                posicion()
+            posicion()
         if eleccion==1:
             print"Has elegido el nivel FACIL"
             print""
@@ -19,6 +104,7 @@ def menu():
                     matriz.append([randint(1,3)for i in range(9)])
             for i in matriz:
                 print i
+            posicion()
         if eleccion==2:
             print"Has elegido el nivel INTERMEDIO"
             print""
@@ -26,13 +112,15 @@ def menu():
                     matriz.append([randint(1,4)for i in range(9)])
             for i in matriz:
                 print i
+            posicion()
         if eleccion==3:
             print"Has elegido el nivel DIFICIL"
             print""
             for i in range(9):
                     matriz.append([randint(1,5)for i in range(9)])
             for i in matriz:
-                print i            
+                print i    
+            posicion()        
         if eleccion==4:
                 def tablerofijo():
                         print"Elija una de las opciones del menu:"
@@ -50,21 +138,24 @@ def menu():
                         if e4==1:
                                 print"Has elegido el cuadrado con 3 colores:"
                                 print""
-                                cuadrado=[[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,3,3,3,3,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,1,2,1,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,3,3,3,3,2,1],[1,2,2,2,2,2,2,2,1],[1,1,1,1,1,1,1,1,1]]
-                                for i in cuadrado:
+                                matriz=[[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,3,3,3,3,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,1,2,1,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,3,3,3,3,2,1],[1,2,2,2,2,2,2,2,1],[1,1,1,1,1,1,1,1,1]]
+                                for i in matriz:
                                         print i
+                                posicion()
                         if e4==2:
                                 print"Has elegido el rombo de 4 colores"
                                 print""
-                                rombo=[[4,4,4,4,1,4,4,4,4],[4,4,4,1,2,1,4,4,4],[4,4,1,2,3,2,1,4,4],[4,1,2,3,1,3,2,1,4],[1,2,3,1,2,1,3,2,1],[4,1,2,3,1,3,2,1,4],[4,4,1,2,3,2,1,4,4],[4,4,4,1,2,1,4,4,4],[4,4,4,4,1,4,4,4,4]]                            
-                                for i in rombo:
+                                matriz=[[4,4,4,4,1,4,4,4,4],[4,4,4,1,2,1,4,4,4],[4,4,1,2,3,2,1,4,4],[4,1,2,3,1,3,2,1,4],[1,2,3,1,2,1,3,2,1],[4,1,2,3,1,3,2,1,4],[4,4,1,2,3,2,1,4,4],[4,4,4,1,2,1,4,4,4],[4,4,4,4,1,4,4,4,4]]                            
+                                for i in matriz:
                                         print i
+                                posicion()
                         if e4==3:
                                 print"Has escogido el casi-damero de 2 colores:"
                                 print""
-                                casidamero=[[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1]]
-                                for i in casidamero:
+                                matriz=[[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1],[2,1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2,1]]
+                                for i in matriz:
                                         print i
+                                posicion()
                         if e4==0:
                                 menu()
                 tablerofijo()
@@ -75,10 +166,15 @@ def menu():
             salir=int(raw_input())
             if salir==2:
                 menu()
+            if salir<1:
+                print"Esa opcion no existe"
+                menu()
+            if salir>2:
+                print"Esa opcion no existe"
+                menu()
         while eleccion>6:
                 print"Esa opcion no existe"
                 menu()
         while eleccion<0:
                 print"Esa opcion no existe"
-                menu()
 menu()
