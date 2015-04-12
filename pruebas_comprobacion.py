a97591ee137c406079a1d03fce4ca6f697a3f07a
villoldin <villoldoserranod@hotmail.com>
2015-04-12 12:09:38 +0200

    Update pruebas_comprobacion.py

--- pruebas_comprobacion.py
+++ pruebas_comprobacion.py
@@ -1,45 +1,88 @@
 import random
 from random import randint
 matriz=[]
-for i in range(9):
-        matriz.append([randint(1,3)for i in range(9)])
+matriz=[[1,1,1,1,1,1,1,1,1],[1,2,2,2,2,2,2,2,1],[1,2,3,3,3,3,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,1,2,1,3,2,1],[1,2,3,1,1,1,3,2,1],[1,2,3,3,3,3,3,2,1],[1,2,2,2,2,2,2,2,1],[1,1,1,1,1,1,1,1,1]]
 for i in matriz:
     print i
-
 def posicion():
     print""
-    print"Introduzca las coordenadas de la posicion:"
-    posiciony=int(raw_input())
-    posicionx=int(raw_input())
-    print"La posicion escogida es:"
-    print matriz[posicionx-1][posiciony-1]
+    print"Introduzca las coordenadas de la posicion."
+    print"Fila:"
+    y=int(raw_input())
+    print"Columna:"
+    x=int(raw_input())
+    if x>9:
+        print"Esa posicion no existe"
+        posicion()
+    if y>9:
+        print"Esa posicion no existe"
+        posicion()
+    if x<1:
+        print"Esa posicion no existe"
+        posicion()
+    if y<1:
+        print"Esa posicion no existe"
+        posicion()
+    print"La posicion escogida es:"    
+    print matriz[y-1][x-1]
     def compro_abajo():
         a=0
-        while matriz[posicionx-1][posiciony-1]==matriz[posicionx][posiciony-1]:
-            a+=1
-            print"correcto"
-            print a
-            matriz[posicionx+a][posiciony-1]=0
+        if matriz[y-1][x-1]==matriz[y][x]:
+            matriz[y-1][x-1]=0
+        while matriz[y-1][x-1]==matriz[y+a+1][x-1]:
+            matriz[y+a][x-1]=0          
+            
+            print""
+            a+=1              
             for i in matriz:
-                print i                     
+                print i          
         posicion()
-    def compro_arriba():    
-        a=1
-        while matriz[posicionx-1][posiciony-1]==matriz[posicionx-1][posiciony-a]:
-            a+=1
+    def compro_arriba():
+        while matriz[y-1][x-1]==matriz[y-2][x-1]:
             print "correcto"
-            matriz[posicionx-1][posiciony-1]=0
+            a=0 #Variable para que se avance la posicion
+            matriz[y-1][x-1]=0            
+            matriz[y-a][x-1]=0
+            a+=1
+            if matriz[y-a][x-1]==matriz[y-(a+1)][x-1]:
+                matriz[y-a][x-1]=0
+                matriz[y-(a+1)][x-1]=0
+                a+=1
+            else:            
+                for i in matriz:
+                    print i
+    def compro_derecha():
+        a=-1
+        while matriz[y-1][x-1]==matriz[y-1][x+a]:
+            matriz[y-1][x-1]=0           
+            matriz[y-1][x+a]=0
+            print"correcto"
+            a+=1
+            if matriz[y-1][x+a]==matriz[y-1][x+a+1]:
+                matriz[y-1][x-1]=0
+                matriz[y-1][x+a]=0
+                matriz[y-1][x+a+1]=0
+                a+=1                       
             for i in matriz:
-                print i
-    compro_abajo()       
-    
+                print i 
+        posicion()
+    def compro_izquierda():
+        a=-1
+        while matriz[y-1][x-1]==matriz[y-1][x-2]:
+            matriz[y-1][x-1]=0           
+            matriz[y-1][x-2]=0
+            print""
+            a+=1
+            if matriz[y-1][x-2]==matriz[y-1][x-a-1]:
+                matriz[y-1][x-1]=0
+                matriz[y-1][x-a]=0
+                matriz[y-1][x-a-1]=0
+                a+=1                       
+            for i in matriz:
+                print i 
+        posicion()
     try:
         compro_abajo()
-    except IndexError():
-        posicion()
-    try:
-        compro_arriba()
     except IndexError:
         posicion()
 posicion()
-
